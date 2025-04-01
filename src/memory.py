9a6545aab531b60ffe7417289252a57ea7adc8e9
Leander Gantert <l.gantert.lg@gmail.com>
2025-04-01 14:42:38 +0200

    implemented datamemory and initialized each bank

--- src/memory.py
+++ src/memory.py
@@ -1,3 +1,70 @@
+# if memory is unknown on power-on reset, the value will be None
+
+
+class Memory:
+    def __init__(self):
+        self.bank0 = [[0] * 8] * 128  
+        self.bank1 = [[0] * 8] * 128  
+        
+        self.initBank0()
+        self.initBank1()
+
+        self.memory = []
+
+        self.memory[0] = self.bank0
+        self.memory[1] = self.bank1     
+
+    def initBank0(self):
+        # initialize Bank 0
+        # TMR0 is unknown
+        self.bank0[0x01] = [None] * 8
+
+        # STATUS
+        self.bank0[0x03] = [0, 0, 0, 1, 1, None, None, None]
+
+        # FSR
+        self.bank0[0x04] = [None] * 8
+
+        # PORTA
+        self.bank0[0x05] = [0, 0, 0, None, None, None, None, None]
+
+        # PORTB
+        self.bank0[0x06] = [None] * 8
+
+        # EEDATA
+        self.bank0[0x08] = [None] * 8
+
+        # EEADR
+        self.bank0[0x09] = [None] * 8
+
+        # PCLATH
+        self.bank0[0x0A] = [0] * 8
+
+        # INTCON
+        self.bank0[0x0B] = [0, 0, 0, 0, 0, 0, 0, None]
+
+    def initBank1(self):
+        # OPTION_REG
+        self.bank1[0x01] = [1] * 8
+
+        # STATUS
+        self.bank1[0x03] = [0, 0, 0, 1, 1, None, None, None]
+
+        # FSR
+        self.bank1[0x04] = [None] * 8
+
+        # TRISA
+        self.bank1[0x05] = [0, 0, 0, 1, 1, 1, 1, 1]
+
+        # TRISB
+        self.bank1[0x06] = [1] * 8
+
+        # EECON1
+        self.bank1[0x08] = [0, 0, 0, 0, None, 0, 0, 0]
+
+        # INTCON
+        self.bank1[0x0B] = [0, 0, 0, 0, 0, 0, 0, None]
+
 class ProgramMemory:
     def __init__(self, size=1024):
         self.size = size
@@ -20,11 +87,11 @@ class ProgramMemory:
 
 class Stack:
     def __init__(self, size=8):
-        self.size = size
+        self.maxSize = size
         self.stack = []
     
     def push(self, value):
-        if len(self.stack) >= self.size:
+        if len(self.stack) >= self.maxSize:
             # raise OverflowError("Stack overflow")
             self.stack.pop(0)
             self.stack.append(value)
